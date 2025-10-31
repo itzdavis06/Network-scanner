7da18b5094d0585ad937d05eecdefdb76a4c606e
itzdavis06 <ebubeobi122006@gmail.com>
2025-10-31 04:02:02 +0000

    did a little trial on searching host

--- main.py
+++ main.py
@@ -10,12 +10,9 @@ port_pattern=re.compile(r"([0-9]+)-([0-9]+)$")
 port_min=0
 port_max=65535
 
-
-openPorts=[]
-
 if __name__=='__main__':
     while True:
-        ipEntered = input('Enter valid ip address to scan: ')
+        ipEntered = input('Enter valid ip address to scan within 255.255.255.255 : ')
         if ip_pattern.search(ipEntered):
             print(f'{ipEntered} is a valid ip address')
             break
@@ -31,16 +28,15 @@ if __name__=='__main__':
 
 
     nm=nmap.PortScanner()
-
-    for port in range(port_min,port_max + 1):
-        try:
-            result=nm.scan(ipEntered,str(port))
-           # print(result)
-
+    try:
+        result=nm.scan(ipEntered,f'{port_min}-{port_max}')
+        for port in result['scan'][ipEntered]['tcp']:
             portStatus=(result['scan'][ipEntered]['tcp'][port]['state'])
             print(f'port {port} is {portStatus}')
-        except Exception as e:
-            print(f'Cannot scan port {port}: {e}')
+    except Exception as e:
+        print(f'cannot scan port:{e}')
+
+
 
 
    
